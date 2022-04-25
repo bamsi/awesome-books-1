b741cd09f907ea97a7286727c215cd4337951727
Haji Bamsi <hibamsi@gmail.com>
2022-04-25 15:24:29 +0300

    fix javascript linter errors

--- modules/display-date.module.js
+++ modules/display-date.module.js
@@ -1,9 +1,10 @@
-import { DateTime } from "../modules/luxon.min.js";
+/* eslint import/prefer-default-export: "off" */
+import { DateTime } from './luxon.min.js';
 
 function todayDate() {
   const dt = DateTime.now();
   const dateformat = `${dt.day} - ${dt.month} - ${dt.year} (${dt.zoneName})`;
-  document.getElementById("current-date").innerHTML = dateformat;
+  document.getElementById('current-date').innerHTML = dateformat;
 }
 
 export { todayDate };
--- script/index.js
+++ script/index.js
@@ -1,21 +1,31 @@
-import { Book } from "../modules/books.module.js";
-import { todayDate } from "../modules/display-date.module.js";
+import { Book } from '../modules/books.module.js';
+import { todayDate } from '../modules/display-date.module.js';
 
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const form = document.getElementById("add-form");
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const form = document.getElementById('add-form');
+const links = document.querySelectorAll('.link-item');
 
 todayDate();
 
-form.addEventListener("submit", () => {
+function hideAllSections() {
+  links.forEach((item) => {
+    const value = item.getAttribute('href');
+    const element = document.querySelector(value);
+    element.classList.add('hide');
+    item.classList.remove('active-link');
+  });
+}
+
+form.addEventListener('submit', () => {
   const newBook = new Book(title.value, author.value);
   newBook.saveBook();
   newBook.displayBooks();
   hideAllSections();
-  const link = document.getElementById("default-link");
-  link.classList.add("active-link");
-  const section = document.querySelector("#list");
-  section.classList.remove("hide");
+  const link = document.getElementById('default-link');
+  link.classList.add('active-link');
+  const section = document.querySelector('#list');
+  section.classList.remove('hide');
   form.reset();
 });
 
@@ -24,34 +34,23 @@ window.onload = book.displayBooks();
 
 function removeBook(index) {
   const removeBooks = new Book();
-  removeBooks.deleteBook(parseInt(index));
+  removeBooks.deleteBook(parseInt(index, 10));
   removeBooks.displayBooks();
 }
 
-const bookItem = document.querySelectorAll(".remove-button");
+const bookItem = document.querySelectorAll('.remove-button');
 bookItem.forEach((e) => {
-  e.addEventListener("click", () => {
-    removeBook(e.getAttribute("value"));
+  e.addEventListener('click', () => {
+    removeBook(e.getAttribute('value'));
   });
 });
-
-const links = document.querySelectorAll(".link-item");
 
 links.forEach((e) => {
-  e.addEventListener("click", () => {
+  e.addEventListener('click', () => {
     hideAllSections();
-    const attributeValue = e.getAttribute("href");
+    const attributeValue = e.getAttribute('href');
     const section = document.querySelector(attributeValue);
-    section.classList.remove("hide");
-    e.classList.add("active-link");
+    section.classList.remove('hide');
+    e.classList.add('active-link');
   });
 });
-
-function hideAllSections() {
-  links.forEach((item) => {
-    const value = item.getAttribute("href");
-    const element = document.querySelector(value);
-    element.classList.add("hide");
-    item.classList.remove("active-link");
-  });
-}
