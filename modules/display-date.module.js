4d35a8472058b1bea3aa2ec7d9502b223d4e84cd
Haji Bamsi <hibamsi@gmail.com>
2022-04-25 14:57:04 +0300

    add link to list after add book

--- modules/display-date.module.js
+++ modules/display-date.module.js
@@ -3,7 +3,7 @@ import { DateTime } from "../modules/luxon.min.js";
 function todayDate() {
   const dt = DateTime.now();
   const dateformat = `${dt.day} - ${dt.month} - ${dt.year} (${dt.zoneName})`;
-  document.getElementById("date").innerHTML = dateformat;
+  document.getElementById("current-date").innerHTML = dateformat;
 }
 
 export { todayDate };
--- script/index.js
+++ script/index.js
@@ -5,16 +5,22 @@ const title = document.getElementById("title");
 const author = document.getElementById("author");
 const form = document.getElementById("add-form");
 
+todayDate();
+
 form.addEventListener("submit", () => {
   const newBook = new Book(title.value, author.value);
   newBook.saveBook();
+  newBook.displayBooks();
+  hideAllSections();
+  const link = document.getElementById("default-link");
+  link.classList.add("active-link");
+  const section = document.querySelector("#list");
+  section.classList.remove("hide");
+  form.reset();
 });
 
 const book = new Book();
-window.onload = function () {
-  book.displayBooks();
-  todayDate();
-};
+window.onload = book.displayBooks();
 
 function removeBook(index) {
   const removeBooks = new Book();
@@ -33,15 +39,19 @@ const links = document.querySelectorAll(".link-item");
 
 links.forEach((e) => {
   e.addEventListener("click", () => {
-    links.forEach((item) => {
-      const value = item.getAttribute("href");
-      const element = document.querySelector(value);
-      element.classList.add("hide");
-      item.classList.remove("active-link");
-    });
+    hideAllSections();
     const attributeValue = e.getAttribute("href");
     const section = document.querySelector(attributeValue);
     section.classList.remove("hide");
     e.classList.add("active-link");
   });
 });
+
+function hideAllSections() {
+  links.forEach((item) => {
+    const value = item.getAttribute("href");
+    const element = document.querySelector(value);
+    element.classList.add("hide");
+    item.classList.remove("active-link");
+  });
+}
